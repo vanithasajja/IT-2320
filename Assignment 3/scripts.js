3139cf4062c4e64c3b11669539219a9c06dee102
vanithasajja <vanitha_1010@yahoo.com>
2017-09-18 16:49:28 -0400

    Assignment 3

---
+++ Assignment 3/scripts.js
@@ -0,0 +1,15 @@
+function welcome()
+
+	{
+
+	document.getElementById("demo").addEventListener("click", display);
+
+	function display()
+
+	{
+
+	alert("Welcome to my Web Page! \n You can reach me at (216)392-7030 if you have any questions.");
+
+	}
+
+	}
