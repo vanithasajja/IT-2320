fc46c4f7f7d88b4b2128550b5bd2923cbc14713b
vanithasajja <vanitha_1010@yahoo.com>
2017-10-09 23:12:28 -0400

    Added Assignment 5 with Yellow Highlight Color for Cells.

---
+++ Assignment 5/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,43 @@
+﻿var sourceDiv = null;
+
+$(document).ready(function () {
+
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++) {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+
+    $(".cell").click(function () {
+        if (sourceDiv == null) {
+            if ($(this).hasClass("piece")) {
+                $(this).addClass("highlight");
+                sourceDiv = this;
+            }
+        }
+        else {
+            if (!$(this).hasClass("piece")) {
+                $(sourceDiv).removeClass("highlight");
+                this.className = sourceDiv.className;
+                sourceDiv.className = "cell";
+                sourceDiv = null;
+            }
+        }
+        
+    });
+    
+});
+
+
+
+
+
+
+
+
+
