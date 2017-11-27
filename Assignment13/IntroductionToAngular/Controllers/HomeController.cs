e641891ca6f59af60a2e1c6b4d409a7a7dafb0dc
vanithasajja <vanitha_1010@yahoo.com>
2017-11-27 15:16:28 -0500

    Assignment 13

---
+++ Assignment13/IntroductionToAngular/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using IntroductionToAngular.Extensions;
+using System.Collections.Generic;
+
+namespace IntroductionToAngular.Controllers
+{
+    public class HomeController : Controller
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        [HttpGet]
+        public JsonResult GetBattingOrder()
+        {
+            return new JsonResult().SerializeObject(new
+            {
+                BattingOrder = new[] {
+                    new { FirstName = "Kenny" , LastName = "Lofton" },
+                    new { FirstName = "Omar" , LastName = "Vizquel" },
+                    new { FirstName = "Carlos" , LastName = "Baerga" },
+                    new { FirstName = "Albert" , LastName = "Belle" },
+                    new { FirstName = "Eddie" , LastName = "Murray" },
+                    new { FirstName = "Jim" , LastName = "Thome" },
+                    new { FirstName = "Manny" , LastName = "Ramirez" },
+                    new { FirstName = "Paul" , LastName = "Sorrento" },
+                    new { FirstName = "Sandy" , LastName = "Alomar" }
+                }
+            });
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
---
+++ Assignment13/IntroductionToAngular/Scripts/Home.js
@@ -0,0 +1,6 @@
+﻿angular.module("my-module", ["ng"])
+  .controller("my-root-controller", ["$scope", "$http", function ($scope, $http) {
+      $http.get("http://localhost:52025/Home/GetBattingOrder").then(function (data) {
+          $scope.players = JSON.parse(data.data).BattingOrder;
+      });
+  }]);
